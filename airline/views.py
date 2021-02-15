0f1e3b245979823d8e5b839fa86e18070985c8da
viralsir <viralsir2018@gmail.com>
2021-02-15 15:57:57 +0530

    passenger and non passenger details are added.

--- airline/views.py
+++ airline/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import flight
+from django.shortcuts import render,redirect
+from .models import flight,passenger
 # Create your views here.
 def home(request):
 
@@ -8,6 +8,17 @@ def home(request):
     })
 
 def flight_details(request,flight_id):
+    flight_info=flight.objects.get(id=flight_id)
     return render(request,"airline/details.html",{
-        "flight_info":flight.objects.get(id=flight_id)
+        "flight_info":flight_info,
+        "Passenger_list":flight_info.passenger.all(),
+        "Non_passenger_list":passenger.objects.exclude(flights=flight_info).all()
     })
+
+def book(request,flight_id):
+    if request.method == 'POST':
+        flight_info=flight.objects.get(id=flight_id)
+        passenger_info=passenger.objects.get(id=request.POST['passenger'])
+        passenger_info.flights.add(flight_info)
+        passenger_info.save()
+        return redirect('home')
